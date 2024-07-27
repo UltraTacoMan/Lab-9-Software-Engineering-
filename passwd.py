62fcf0372365b5d09a2cbb465941ce0e87a547bb
Lymee <slimylymee@gmail.com>
2024-07-27 15:25:11 -0400

    added comments

--- passwd.py
+++ passwd.py
@@ -1,8 +1,8 @@
-#encode
+#encoder
 def encode(password):
     encoded_password = ''.join(str((int(char) + 3) % 10) for char in password)
     return encoded_password
-#decode
+#decoder
 def decode(password):
     decoded_password = ''.join(str((int(char) - 3) % 10) for char in password)
     return decoded_password
